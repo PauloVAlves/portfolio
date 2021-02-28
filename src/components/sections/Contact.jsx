49fcbd68ae5d33aecc63cb16091f97a470e822a2
PauloVAlves <contato@pauloalves.me>
2021-02-28 12:28:56 -0300

    created contact form email functions

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,6 +1,68 @@
+import axios from 'axios';
+import { useState } from 'react/cjs/react.development';
 import styled from 'styled-components';
 
 const Contact = () => {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    message: '',
+    sent: false,
+    err: '',
+    buttonValue: 'Send',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const formSublit = (e) => {
+    e.preventDefault();
+
+    setData({ ...data, buttonValue: 'Sending...' });
+
+    axios
+      .post('/sendmail', data)
+      .then((res) => {
+        if (res.data.result !== 'success') {
+          setData({
+            ...data,
+            buttonValue: 'Failed to send',
+            sent: false,
+            err: 'fail',
+          });
+          setTimeout(() => {
+            resetForm();
+          }, 6000);
+        } else {
+          setData({
+            ...data,
+            sent: true,
+            buttonValue: 'Email Sent!',
+            err: 'success',
+          });
+          setTimeout(() => {
+            resetForm();
+          }, 6000);
+        }
+      })
+      .catch((err) => {
+        setData({ ...data, buttonValue: 'Failed to send', err: 'fail' });
+      });
+  };
+
+  const resetForm = () => {
+    setData({
+      name: '',
+      email: '',
+      message: '',
+      sent: false,
+      buttonValue: 'Send',
+      err: '',
+    });
+  };
+
   return (
     <ContactMe
       method='post'
@@ -11,12 +73,33 @@ const Contact = () => {
       <h2>Contact</h2>
       <input type='hidden' name='form-name' value='contact' />
       <label htmlFor='name'>Name</label>
-      <input type='text' name='name' id='name' />
+      <input
+        type='text'
+        name='name'
+        id='name'
+        value={data.name}
+        onChange={handleChange}
+      />
       <label htmlFor='email'>Email</label>
-      <input type='email' name='email' id='email' />
+      <input
+        type='email'
+        name='email'
+        id='email'
+        value={data.email}
+        onChange={handleChange}
+      />
       <label htmlFor='message'>Message</label>
-      <textarea name='message' id='message' cols='30' rows='10'></textarea>
-      <input type='submit' value='Send' className='btn btn-send' />
+      <textarea
+        name='message'
+        id='message'
+        cols='30'
+        rows='10'
+        value={data.message}
+        onChange={handleChange}
+      ></textarea>
+      <button onClick={formSublit} className='btn btn-send'>
+        {data.buttonValue}
+      </button>
     </ContactMe>
   );
 };
@@ -40,7 +123,7 @@ const ContactMe = styled.form`
     margin-left: auto;
     width: 40%;
     margin-bottom: 20px;
-    font-size: 1.3rem;
+    font-size: 1.1rem;
     border-radius: 10px;
     padding: 10px;
     border: none;
@@ -48,7 +131,11 @@ const ContactMe = styled.form`
 
   .btn {
     transition: all 1s ease-in-out;
+    border: none;
+    border-radius: 10px;
+    padding: 10px;
     cursor: pointer;
+    font-size: 1.3rem;
     text-align: center;
     background-color: #141738;
     color: #f6b011;
